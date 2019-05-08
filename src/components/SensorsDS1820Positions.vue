71efae5fe2da8cedcc2178dfab92bbc0adf39469
Vincent Kuhlen <vkuhlen@uni-bremen.de>
2019-05-08 14:56:10 +0200

    change one label

--- src/components/SensorsDS1820Positions.vue
+++ src/components/SensorsDS1820Positions.vue
@@ -6,7 +6,8 @@
                     :name="position"
                     :class="[ds1820_positions[position] ? 'assigned' : 'unassigned']"
                     @click="$emit('assign', position)">
-                    {{ temperatures[position] | toFixed(1) | ifEmpty(idx+1) }}</button>
+                    {{ get_label(temperatures[position], idx) }}
+            </button>
         </div>
     </fieldset>
 </template>
@@ -18,6 +19,20 @@ export default {
         ds1820_positions: Object,
         temperatures: Object
     },
+    methods: {
+        get_label(value, idx) {
+            let label = (!Number.isNaN(value) && Number.isFinite(value)
+                        ) ? Number.parseFloat(value).toFixed(1) : ''
+            let alternative = "";
+            if (idx === 5) {
+                alternative = "out";
+            }
+            else {
+                alternative = idx + 1;
+            }
+            return (label) ? label : alternative;
+        } 
+    },
     data() {
         return {
             allowed_positions: [
@@ -26,7 +41,7 @@ export default {
                 't_i_3',
                 't_i_4',
                 't_i_5',
-                't_i_6'
+                't_o'
             ]
         }
     }
